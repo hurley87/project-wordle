eec019eecb08c31f4b215c56c537bea16a534f73
David Hurley <dhurls99@gmail.com>
2023-09-21 06:26:19 -0400

    Exercise 4: Game logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessInput from '../GuessInput/GuessInput';
+import Guesses from '../Guesses/Guesses';
+import { checkGuess } from '../../game-helpers';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +12,19 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleAddGuess(word) {
+    const letters = checkGuess(word, answer)
+    const guessWithLetters = {
+      id: Math.random(),
+      word,
+      letters
+    }
+    setGuesses((guesses) => [...guesses, guessWithLetters]);
+  }
+  return <><Guesses guesses={guesses} /><GuessInput handleAddGuess={handleAddGuess} /></>;
 }
 
 export default Game;
+
